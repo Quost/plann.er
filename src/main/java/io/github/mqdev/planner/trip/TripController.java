281503143d08d5a3913cafd571f41b88c17f31d9
Matheus Quost <matheusquost@gmail.com>
2024-07-09 20:29:35 -0300

    chore: Add trip creation endpoint to TripController

--- src/main/java/io/github/mqdev/planner/trip/TripController.java
+++ src/main/java/io/github/mqdev/planner/trip/TripController.java
@@ -1,17 +1,28 @@
 package io.github.mqdev.planner.trip;
 
+import io.github.mqdev.planner.participant.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/trips")
 public class TripController {
 
-    @PostMapping("/trips")
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @PostMapping
     public ResponseEntity<Trip> createTrip(@RequestBody TripRequestPayload tripRequest) {
         try {
             Trip trip = new Trip(tripRequest);
-            return ResponseEntity.ok(trip);
+            var savedTrip = tripRepository.save(trip);
+            participantService.registerParticipantsToTrip(tripRequest.emails_to_invite(), savedTrip.getId());
+            return ResponseEntity.ok(savedTrip);
         } catch (Exception e) {
             System.out.println(e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
