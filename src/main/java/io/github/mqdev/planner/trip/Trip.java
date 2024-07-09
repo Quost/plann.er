984687fb25f9192dc4fdae2c390a1908d8dda7c6
Matheus Quost <matheusquost@gmail.com>
2024-07-09 19:46:05 -0300

    chore: Add Trip entity with necessary fields and constructors

--- src/main/java/io/github/mqdev/planner/trip/Trip.java
+++ src/main/java/io/github/mqdev/planner/trip/Trip.java
@@ -1,2 +1,53 @@
-package io.github.mqdev.planner.trip;public class Trip {
+package io.github.mqdev.planner.trip;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public Trip(TripRequestPayload payload) {
+        this.destination = payload.destination();
+        this.isConfirmed = false;
+        this.startsAt = LocalDateTime.parse(payload.startsAt(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(payload.endsAt(), DateTimeFormatter.ISO_DATE_TIME);
+        this.ownerName = payload.ownerName();
+        this.ownerEmail = payload.ownerEmail();
+        this.createdAt =LocalDateTime.now();
+    }
 }
