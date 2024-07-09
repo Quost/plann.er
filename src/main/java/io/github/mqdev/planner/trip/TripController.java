324867606698463abb2c35c225a6e05f61109ddf
Matheus Quost <matheusquost@gmail.com>
2024-07-09 20:15:43 -0300

    chore: Add  handle of trip creation to TripController

--- src/main/java/io/github/mqdev/planner/trip/TripController.java
+++ src/main/java/io/github/mqdev/planner/trip/TripController.java
@@ -1,2 +1,20 @@
-package io.github.mqdev.planner.trip;public class TripController {
+package io.github.mqdev.planner.trip;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TripController {
+
+    @PostMapping("/trips")
+    public ResponseEntity<Trip> createTrip(@RequestBody TripRequestPayload tripRequest) {
+        try {
+            Trip trip = new Trip(tripRequest);
+            return ResponseEntity.ok(trip);
+        } catch (Exception e) {
+            System.out.println(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
 }
