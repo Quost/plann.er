d8a99776b10cbbba12c96dc770dffd5559afad22
Matheus Quost <matheusquost@gmail.com>
2024-07-09 20:29:24 -0300

    chore: Adds register participants to trip and trigger confirmation emails methods to the participant service

--- src/main/java/io/github/mqdev/planner/participant/ParticipantService.java
+++ src/main/java/io/github/mqdev/planner/participant/ParticipantService.java
@@ -1,2 +1,14 @@
-package io.github.mqdev.planner.participant;public class ParticipantService {
+package io.github.mqdev.planner.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    public void registerParticipantsToTrip(List<String> emails, UUID tripId) {}
+
+    public void triggerConfirmationEmailsToParticipants(UUID tripId) {}
 }
