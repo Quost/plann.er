27efcab5bf38ebe19c112b0e1a6414691ff85802
Matheus Quost <matheusquost@gmail.com>
2024-07-10 23:43:41 -0300

    feat: Add Activity entity and table
    This commit adds a new entity called Activity, along with its corresponding table in the database schema. The Activity entity includes fields for ID, occursAt, title, and a reference to the Trip entity. This change allows for managing activities in the planner project.

---
+++ src/main/java/io/github/mqdev/planner/activities/Activity.java
@@ -0,0 +1,37 @@
+package io.github.mqdev.planner.activities;
+
+import io.github.mqdev.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
