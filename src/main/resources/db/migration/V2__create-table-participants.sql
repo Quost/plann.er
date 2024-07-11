a66b7c80bc2558cebcac9a2e68e16f92a636e69d
Matheus Quost <matheusquost@gmail.com>
2024-07-10 22:04:12 -0300

    feat: Add participants table to database schema
    This commit adds a new table called "participants" to the database schema. The table includes columns for participant ID, trip ID, name, email, confirmation status, and creation timestamp. The "participants" table is referenced by the "trips" table.

---
+++ src/main/resources/db/migration/V2__create-table-participants.sql
@@ -0,0 +1,9 @@
+CREATE TABLE participants (
+    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    trip_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    is_confirmed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+)
