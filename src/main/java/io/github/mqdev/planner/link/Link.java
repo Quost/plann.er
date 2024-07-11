a7533f5f4b85b58c304b216697cf61621ddabd9b
Matheus Quost <matheusquost@gmail.com>
2024-07-11 00:16:08 -0300

    feat: Add Link entity and table
    This commit adds a new entity called Link, along with its corresponding table in the database schema. The Link entity includes fields for ID, url, title, and a reference to the Trip entity. This change allows for managing links in the planner project.

---
+++ src/main/java/io/github/mqdev/planner/link/Link.java
@@ -0,0 +1,38 @@
+package io.github.mqdev.planner.link;
+
+import io.github.mqdev.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "links")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Link {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
